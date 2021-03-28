9429e543d64c46ff9389c2ce5c2a9e530546d01b
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2021-03-28 14:51:25 +0300

    lisätty skeleton vaikeusaste inputille (radio list)

--- app/src/main/java/com/example/mobiiliohjelmointi_lopputy/SettingsActivity.java
+++ app/src/main/java/com/example/mobiiliohjelmointi_lopputy/SettingsActivity.java
@@ -9,23 +9,16 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
-    private String m_selectedCategory = "";
+    private String m_category = "";
+    private String m_difficulty = "Any Difficulty";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        /* Click on select category header creates radioinput list dialog */
-        TextView selectCategory_TextView = (TextView)findViewById(R.id.selectCategory_TextView);
-        selectCategory_TextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                selectQuizCategory_Dialog();
-            }
-        });
-
-
+        // Only run once on start up, initializes all needed functions
+        initializeAllClickListenersOnStart();
     }
 
     /* Select right category for API call */
@@ -39,18 +32,72 @@ public class SettingsActivity extends AppCompatActivity {
         selectCategory_Dialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int selectedItem) {
-                m_selectedCategory = items[selectedItem];
+                m_category = items[selectedItem];
+                TextView selectedItem_TextView = (TextView)findViewById(R.id.selectedItemCate_TextView);
+                selectedItem_TextView.setText(m_category);
                // Toast.makeText( SettingsActivity.this, m_selectedCategory, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        selectCategory_Dialog.setPositiveButton("Select", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
             }
         });
 
         selectCategory_Dialog.show();
+    }
+
+    private void selectDifficultyDialog() {
+        String[] items = {"Any Difficulty", "Easy", "Medium", "Hard"};
+
+        AlertDialog.Builder inputDialog = new AlertDialog.Builder(this );
+        inputDialog.setTitle("Choose difficulty");
+
+        inputDialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int selectedItem) {
+                m_difficulty = items[selectedItem];
+                TextView selectedDifficulty_TextView = (TextView)findViewById(R.id.selectedDifficultyItem_TextView);
+                selectedDifficulty_TextView.setText(m_difficulty);
+                // Toast.makeText( SettingsActivity.this, m_selectedCategory, Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
+        });
+
+        inputDialog.show();
+    }
+
+    private void initializeAllClickListenersOnStart() {
+        /* Click on select category header creates radio input list dialog */
+        TextView selectCategory_TextView = (TextView)findViewById(R.id.selectCategory_TextView);
+        selectCategory_TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectQuizCategory_Dialog();
+            }
+        });
+
+        /* Click on selected category item creates radio input list dialog */
+        TextView selectedCategoryItem_TextView = (TextView)findViewById(R.id.selectedItemCate_TextView);
+        selectedCategoryItem_TextView.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectQuizCategory_Dialog();
+            }
+        });
+
+        /* Click on select difficulty header creates input dialog */
+        TextView selectDifficultyHeader_TextView = (TextView)findViewById(R.id.selectDifficultyHead_TextView);
+        selectDifficultyHeader_TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectDifficultyDialog();
+            }
+        });
+
+        /* Click on selected difficulty item creates input dialog */
+        TextView selectedDifficultyItem = (TextView)findViewById(R.id.selectedDifficultyItem_TextView);
+        selectedDifficultyItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectDifficultyDialog();
+            }
+        });
     }
 }
