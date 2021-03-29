8f774103705531e838590eed9dcb8b17748d92e7
Jantero93 <janzku1993@hotmail.com>
2021-03-29 16:11:45 +0300

    settings activity UI periaatteessa valmis

---
+++ app/src/main/java/com/example/mobiiliohjelmointi_lopputy/DataAPI.java
@@ -0,0 +1,29 @@
+package com.example.mobiiliohjelmointi_lopputy;
+
+public class DataAPI {
+    private String M_apiLink = "https://opentdb.com/api.php?";
+    private String M_difficulty = "";
+    private int M_amount = 0;
+    private int M_categoryId = 0;
+
+    public void setM_difficulty(String difficulty) {
+
+    }
+
+    public void setM_amount(int amount) {
+
+    }
+
+    public void setM_categoryId(String category){
+
+    }
+
+    public String[] getAllCategorysAPI() {
+        String[] test = {"nmu","test"};
+
+        return test;
+    }
+
+
+
+}
