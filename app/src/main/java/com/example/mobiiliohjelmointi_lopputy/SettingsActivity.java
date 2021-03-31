397139c69a04b7c9fb344d4fc33d643dbb3f0dfa
Jantero93 <janzku1993@hotmail.com>
2021-03-31 10:00:04 +0300

    settings act tarvii vielä kategoriat

--- app/src/main/java/com/example/mobiiliohjelmointi_lopputy/SettingsActivity.java
+++ app/src/main/java/com/example/mobiiliohjelmointi_lopputy/SettingsActivity.java
@@ -3,7 +3,9 @@ package com.example.mobiiliohjelmointi_lopputy;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +13,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Arrays;
 
 public class SettingsActivity extends AppCompatActivity {
+    private int m_amount = 10;
     private String m_category = "";
     private String m_difficulty = "Any Difficulty";
     private String m_type = "Any Type";
-    private DataAPI m_api;
+    private DataAPI data = DataAPI.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        m_api = new DataAPI();
+        //create new singleton data class
+
+
         // Only run once on start up, initializes all needed functions
         initializeAllClickListenersOnStart();
     }
@@ -69,6 +74,7 @@ public class SettingsActivity extends AppCompatActivity {
                 TextView selectedDifficulty_TextView = (TextView)findViewById(R.id.selectedDifficultyItem_TextView);
                 selectedDifficulty_TextView.setText(m_difficulty);
                 // Toast.makeText( SettingsActivity.this, m_selectedCategory, Toast.LENGTH_LONG).show();
+
                 dialog.dismiss();
             }
         });
@@ -77,11 +83,12 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     private void selectTypeDialog() {
-        String[] items = {"Any Type", "Multiple Choice", "True / False"};
+       String[] items = {"Any Type", "Multiple Choice", "True / False"};
 
         AlertDialog.Builder inputDialog = new AlertDialog.Builder( this );
         inputDialog.setTitle("Choose Type");
 
+        // set checked item which was last time selected/default
         int checkedItem = Arrays.asList(items).indexOf(m_type);
 
         inputDialog.setSingleChoiceItems(items, checkedItem, new DialogInterface.OnClickListener() {
@@ -152,5 +159,62 @@ public class SettingsActivity extends AppCompatActivity {
                 selectTypeDialog();
             }
         });
+    }
+
+    //generate api link, make api call and return to main menu
+    public void saveSettings(View view) {
+
+        // order for api link question base link, amount, category id, difficulty, type (boolean / multiple)
+        String apiLink = data.getApiLink();
+        // get amount from text edit
+        EditText numInput = (EditText)findViewById(R.id.numberOfQuestions_editNumber);
+        String linkAmount = "amount=" + numInput.getText();
+
+        String linkDiff = "";
+        // difficulty on right format
+        switch (m_difficulty)
+        {
+            case "Any Difficulty":
+                linkDiff = "";
+                break;
+            case "Easy":
+                linkDiff = "&difficulty=easy";
+                break;
+            case "Medium":
+                linkDiff = "&difficulty=medium";
+                break;
+            case "Hard":
+                linkDiff = "&difficulty=hard";
+                break;
+            default: linkDiff = "";
+                break;
+        }
+
+        // multiple questions / true & false on right format
+        String linkType = "";
+        switch (m_type)
+        {
+            case "Any Type":
+                linkType = "";
+                break;
+            case "Multiple Choice":
+                linkType = "&type=multiple";
+                break;
+            case "True / False":
+                linkType = "&type=boolean";
+                break;
+            default:
+                linkType = "";
+                break;
+        }
+
+        // generate api link
+        apiLink += linkAmount + linkDiff + linkType;
+
+        Toast.makeText(this,apiLink,Toast.LENGTH_LONG).show();
+
+
+
+
     }
 }
