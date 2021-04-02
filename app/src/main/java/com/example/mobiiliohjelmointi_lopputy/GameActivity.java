0344cfcb5c37c1ec18b660635b01a9a3e56ffe77
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2021-04-02 18:56:39 +0300

    alusettu UI muuttujat gameaktivity. aloitettu vähä gameplay

--- app/src/main/java/com/example/mobiiliohjelmointi_lopputy/GameActivity.java
+++ app/src/main/java/com/example/mobiiliohjelmointi_lopputy/GameActivity.java
@@ -3,6 +3,9 @@ package com.example.mobiiliohjelmointi_lopputy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +27,16 @@ public class GameActivity extends AppCompatActivity {
     RequestQueue m_requestQueue;
     ArrayList<QuizQuestion> m_gameQuestions;
 
+    TextView textView_question;
+    TextView textView_score;
+    TextView textView_questionAnswered;
+    Button button_confirm;
+    RadioButton radioButton1;
+    RadioButton radioButton2;
+    RadioButton radioButton3;
+    RadioButton radioButton4;
+    RadioGroup radioGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +44,9 @@ public class GameActivity extends AppCompatActivity {
         API_Singleton m_api = API_Singleton.getInstance(this);
         m_requestQueue = API_Singleton.getInstance(this).getRequestQueue();
         m_gameQuestions = new ArrayList<>();
+        this.initializeUI();
 
-        // hide ui until data is fetched
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.gameLayout);
-        layout.setVisibility(View.GONE);
+
 
         Intent intent = getIntent();
         // fetch game data, start game after data is fetched
@@ -103,6 +115,33 @@ public class GameActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+
+    }
+
+    /* Initialize UI components onCreate */
+    private void initializeUI() {
+        textView_question = (TextView)findViewById(R.id.text_view_question);
+        textView_score = (TextView)findViewById(R.id.text_view_score);
+        textView_questionAnswered = (TextView)findViewById(R.id.text_view_question_count);
+        button_confirm = (Button)findViewById(R.id.button_confirm_next);
+        radioButton1 = (RadioButton)findViewById(R.id.radio_button1);
+        radioButton2 = (RadioButton)findViewById(R.id.radio_button2);
+        radioButton3 = (RadioButton)findViewById(R.id.radio_button3);
+        radioButton4 = (RadioButton)findViewById(R.id.radio_button4);
+        radioGroup = (RadioGroup)findViewById(R.id.radio_group);
+
+        // hide ui until data is fetched
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.gameLayout);
+        layout.setVisibility(View.GONE);
+    }
+
+    public void confirmButtonClicked(View view) {
+        for (QuizQuestion question : m_gameQuestions) {
+            setUIforQuestion(question);
+        }
+    }
+
+    private void setUIforQuestion(QuizQuestion question) {
 
     }
 }
